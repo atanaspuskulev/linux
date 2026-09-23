0836dcaeb7be1bd77310bafed4fe9ffde79938a0
Atanas Pyuskyulev <atanas.puskulev@gmail.com>
2026-09-23 11:12:51 +0300

    My custom packages list of Ubuntu

---
+++ pkg-apt-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install -y vim \
+    vim-scripts \
+    tree \
+    tmux \
+    stow \
+    python3-pynvim \
+    neovim-which-key \
+    neovim-lua-line \
+    neovim \
+    exuberant-ctags \
+    git \
+    dnsutils
